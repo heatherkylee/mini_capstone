11303e2b9d4ccfc061f489237f84b9f7b46acf16
Heather Lee <heather.k.lee@gmail.com>
2018-06-26 20:45:04 -0400

    create Product controller, route, and view for first product and all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def get_product
+    @product1 = Product.first
+    render "product_view.json.jbuilder"
+  end
+
+  def get_all
+    @all_products = Product.all
+    render "all_products_view.json.jbuilder"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  product = Product.create(name: Faker::Dessert.variety, price: Faker::Number.between(5, 20), description: Faker::Dessert.flavor, image: "")
+  product.save
+end
