45daaf6fe7dc78bd8bfe1d9066ab0eeea7c3aca2
Heather Lee <heather.k.lee@gmail.com>
2018-07-09 22:42:16 -0400

    Create _contact.json.jbuilder file in order to dry code, modify database by adding middle name and bio attributes and implement changes to the frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_235555) do
+ActiveRecord::Schema.define(version: 2018_07_10_022425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 6, scale: 2
     t.string "image"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
