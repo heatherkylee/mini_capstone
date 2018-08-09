0bebee01de9dc08531453fd28160b8017e80594e
Heather Lee <heather.k.lee@gmail.com>
2018-08-08 22:14:19 -0400

    install vue.js

---
+++ public/index.js
@@ -0,0 +1,32 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    console.log("load when the page loads");
+    axios.get("/api/products").then(function(response) {
+      console.log(response.data);
+      this.products = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
